4ad2e2aeb41b4698e84400f5a22715483b843b4a
Alexis Lyndon Galaura <alexislyndon@gmail.com>
2020-02-27 20:35:49 +0800

    cleaned module

--- Module1.vb
+++ Module1.vb
@@ -22,50 +22,10 @@ Module Module1
             Using mStream As New MemoryStream(byteArrayIn)
                 Return Image.FromStream(mStream)
             End Using
+        Else
+            Return Nothing
         End If
     End Function
-
-    Function isvalid(hehe As Add_Visitor) As Boolean
-
-        Dim valid As Boolean = True
-        For Each ctrl In hehe.Controls
-            Dim tb = TryCast(ctrl, TextBox)
-            Dim cb = TryCast(ctrl, ComboBox)
-            If tb IsNot Nothing Then
-                If tb.Text.Trim = "" Then
-                    tb.BackColor = Color.Red
-                    valid = False
-                End If
-            End If
-
-            If cb IsNot Nothing Then
-                If cb.SelectedIndex = -1 Or cb.Text.Trim = "" Then
-                    cb.BackColor = Color.Red
-                    valid = False
-                End If
-            End If
-        Next
-        Return valid
-    End Function
-
-    Public Sub clearer(obj As Add_Visitor)
-        For Each ctrl In obj.Controls
-            Dim tb = TryCast(ctrl, TextBox)
-            Dim cb = TryCast(ctrl, ComboBox)
-            If tb IsNot Nothing Then
-                tb.Clear()
-            End If
-
-            If cb IsNot Nothing Then
-                'cb.Text = ""
-                cb.SelectedIndex = -1
-            End If
-        Next
-        'obj.PictureBox1.Image = Image.FromFile("X:\1902\VB.net\Security Logbook\img\portrait.jpg")
-        IO.Path.Combine(path, "\img\portrait.jpg")
-        obj.PictureBox1.Image = Image.FromFile("X:\1902\VB.net\Security Logbook\img\portrait.jpg")
-        obj.PictureBox2.Image = Image.FromFile("X:\1902\VB.net\Security Logbook\img\blankid.jpg")
-    End Sub
 
     Public Sub clear(obj)
         For Each ctrl In obj.Controls
